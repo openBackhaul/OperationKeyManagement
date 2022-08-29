7ab63abe6bd73847c61cdf0130bdef7390785746
Martin Sunal <martin.sunal@paxet.io>
2022-08-29 12:02:17 +0200

    Implemented StringProfile controller
    Fixes #38
    
    Signed-off-by: Martin Sunal <martin.sunal@paxet.io>

--- server/controllers/StringProfile.js
+++ server/controllers/StringProfile.js
@@ -1,34 +1,46 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
 var StringProfile = require('../service/StringProfileService');
+var responseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
+var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
+var oamLogService = require('onf-core-model-ap/applicationPattern/services/OamLogService');
 
-module.exports.getStringProfileStringName = function getStringProfileStringName (req, res, next) {
-  StringProfile.getStringProfileStringName()
+
+module.exports.getStringProfileStringName = async function getStringProfileStringName(req, res, next) {
+  let responseCode = responseCodeEnum.code.OK;
+  await StringProfile.getStringProfileStringName(req.url)
     .then(function (response) {
-      utils.writeJson(res, response);
+      responseBuilder.buildResponse(res, responseCode, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      responseBuilder.buildResponse(res, responseCode, response);
     });
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.getStringProfileStringValue = function getStringProfileStringValue (req, res, next) {
-  StringProfile.getStringProfileStringValue()
+module.exports.getStringProfileStringValue = async function getStringProfileStringValue(req, res, next) {
+  let responseCode = responseCodeEnum.code.OK;
+  await StringProfile.getStringProfileStringValue(req.url)
     .then(function (response) {
-      utils.writeJson(res, response);
+      responseBuilder.buildResponse(res, responseCode, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      responseBuilder.buildResponse(res, responseCode, response);
     });
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.putStringProfileStringValue = function putStringProfileStringValue (req, res, next, body) {
-  StringProfile.putStringProfileStringValue(body)
+module.exports.putStringProfileStringValue = async function putStringProfileStringValue(req, res, next, body) {
+  let responseCode = responseCodeEnum.code.NO_CONTENT;
+  await StringProfile.putStringProfileStringValue(body, req.url)
     .then(function (response) {
-      utils.writeJson(res, response);
+      responseBuilder.buildResponse(res, responseCode, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      responseBuilder.buildResponse(res, responseCode, response);
     });
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
