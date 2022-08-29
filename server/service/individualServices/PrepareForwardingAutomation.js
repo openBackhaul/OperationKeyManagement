f37cee4d6fa2d41b8efe52de582c0720f5269e62
vanithaValluripalli <75898180+vanithaValluripalli@users.noreply.github.com>
2022-08-29 15:33:31 +0530

    Fixed the indentation issue

--- server/service/individualServices/PrepareForwardingAutomation.js
+++ server/service/individualServices/PrepareForwardingAutomation.js
@@ -1,28 +1,27 @@
 const prepareALTForwardingAutomation = require('onf-core-model-ap-bs/basicServices/services/PrepareALTForwardingAutomation');
 
 exports.bequeathYourDataAndDie = function (logicalTerminationPointconfigurationStatus) {
-    return new Promise(async function (resolve, reject) {
-        let forwardingConstructAutomationList = [];
-        try {
+  return new Promise(async function (resolve, reject) {
+    let forwardingConstructAutomationList = [];
+    try {
+      /***********************************************************************************
+       * forwardings for application layer topology
+       ************************************************************************************/
+      let applicationLayerTopologyForwardingInputList = await prepareALTForwardingAutomation.getALTForwardingAutomationInputAsync(
+        logicalTerminationPointconfigurationStatus,
+        undefined
+      );
 
-            /***********************************************************************************
-             * forwardings for application layer topology
-             ************************************************************************************/
-            let applicationLayerTopologyForwardingInputList = await prepareALTForwardingAutomation.getALTForwardingAutomationInputAsync(
-                logicalTerminationPointconfigurationStatus,
-                undefined
-            );
-
-            if (applicationLayerTopologyForwardingInputList) {
-                for (let i = 0; i < applicationLayerTopologyForwardingInputList.length; i++) {
-                    let applicationLayerTopologyForwardingInput = applicationLayerTopologyForwardingInputList[i];
-                    forwardingConstructAutomationList.push(applicationLayerTopologyForwardingInput);
-                }
-            }
-
-            resolve(forwardingConstructAutomationList);
-        } catch (error) {
-            reject(error);
+      if (applicationLayerTopologyForwardingInputList) {
+        for (let i = 0; i < applicationLayerTopologyForwardingInputList.length; i++) {
+          let applicationLayerTopologyForwardingInput = applicationLayerTopologyForwardingInputList[i];
+          forwardingConstructAutomationList.push(applicationLayerTopologyForwardingInput);
         }
-    });
+      }
+
+      resolve(forwardingConstructAutomationList);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
