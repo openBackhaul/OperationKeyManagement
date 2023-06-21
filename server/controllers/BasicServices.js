5a5f42f8a8529602b7c62c7dd1be3a1326192a5e
ManasaBM <manasa.b-m.external@telefonica.com>
2023-06-21 11:19:23 +0200

    Adding the BadRequestHttpException

--- server/controllers/BasicServices.js
+++ server/controllers/BasicServices.js
@@ -5,7 +5,7 @@ var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server
 var restResponseHeader = require('onf-core-model-ap/applicationPattern/rest/server/ResponseHeader');
 var restResponseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
 var executionAndTraceService = require('onf-core-model-ap/applicationPattern/services/ExecutionAndTraceService');
-
+const BadRequestHttpException = require('onf-core-model-ap/applicationPattern/rest/server/HttpException');
 module.exports.embedYourself = async function embedYourself(req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
   try {
     let startTime = process.hrtime();
@@ -341,7 +341,7 @@ module.exports.updateOperationKey = async function updateOperationKey(req, res, 
       })
       .catch(async function (responseBody) {
         responseBodyToDocument = responseBody;
-        if(responseBody== "OperationClientUuidisnotPresent"|| responseBody == "OperationServerUuidisnotPresent"){
+        if (responseBody instanceof BadRequestHttpException) {
           responseCode = responseCodeEnum.code.BAD_REQUEST;
         }else{
         responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
