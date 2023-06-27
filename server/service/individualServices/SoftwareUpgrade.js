f7ccb07c497d1fdc07102e88194c197d1b2e80f0
Dana Sunalova <dana@paxet.io>
2023-06-27 14:25:24 +0200

    issue-221 : fixed trace-indicator
    Signed-off-by: Dana Sunalova <dana@paxet.io>

--- server/service/individualServices/SoftwareUpgrade.js
+++ server/service/individualServices/SoftwareUpgrade.js
@@ -15,53 +15,25 @@ const OperationServerInterface = require('onf-core-model-ap/applicationPattern/o
 const FcPort = require('onf-core-model-ap/applicationPattern/onfModel/models/FcPort');
 const OperationClientInterface = require('onf-core-model-ap/applicationPattern/onfModel/models/layerProtocols/OperationClientInterface');
 const eventDispatcher = require('onf-core-model-ap/applicationPattern/rest/client/eventDispatcher');
-const ltpClientConstants = require('../../utils/ltpClientConstants');
 const onfModelUtils = require("../../utils/OnfModelUtils");
-
+var traceIncrementer = 1;
 
 /**
  * This method performs the set of procedure to transfer the data from this version to next version 
  * of the application and bring the new release official
- * @param {boolean} isdataTransferRequired represents true if data transfer is required
  * @param {String} user User identifier from the system starting the service call
  * @param {String} xCorrelator UUID for the service execution flow that allows to correlate requests and responses
  * @param {String} traceIndicator Sequence of request numbers along the flow
  * @param {String} customerJourney Holds information supporting customer’s journey to which the execution applies
    @returns {Promise} Promise is resolved if the operation succeeded else the Promise is rejected 
 * **/
-exports.upgradeSoftwareVersion = async function (isdataTransferRequired, user, xCorrelator, traceIndicator, customerJourney) {
-  return new Promise(async function (resolve, reject) {
-    try {
-      if (isdataTransferRequired) {
-        await transferDataToTheNewRelease(user, xCorrelator, traceIndicator, customerJourney);
-      }
-      await redirectNotificationNewRelease(user, xCorrelator, traceIndicator, customerJourney);
-      await replaceOldReleaseWithNewRelease(user, xCorrelator, traceIndicator, customerJourney);
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-/**
- * This method performs the data transfer from the current instance to the new instance
- * @param {String} user User identifier from the system starting the service call
- * @param {String} xCorrelator UUID for the service execution flow that allows to correlate requests and responses
- * @param {String} traceIndicator Sequence of request numbers along the flow
- * @param {String} customerJourney Holds information supporting customer’s journey to which the execution applies
- * The following are the list of forwarding-construct that will be automated to transfer the data from this current release to next release
- * 1. PromptForBequeathingDataCausesTransferOfListOfApplications
- */
-async function transferDataToTheNewRelease(user, xCorrelator, traceIndicator, customerJourney) {
-  return new Promise(async function (resolve, reject) {
-    try {
-      await PromptForBequeathingDataCausesTransferOfListOfApplications(user, xCorrelator, traceIndicator, customerJourney);
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+exports.upgradeSoftwareVersion = async function (user, xCorrelator, traceIndicator, customerJourney, _traceIncrementer) {
+  if (_traceIncrementer !== 0) {
+    traceIncrementer = _traceIncrementer;
+  }
+  await PromptForBequeathingDataCausesTransferOfListOfApplications(user, xCorrelator, traceIndicator, customerJourney);
+  await redirectNotificationNewRelease(user, xCorrelator, traceIndicator, customerJourney);
+  await replaceOldReleaseWithNewRelease(user, xCorrelator, traceIndicator, customerJourney);
 }
 
 /**
@@ -79,18 +51,11 @@ async function transferDataToTheNewRelease(user, xCorrelator, traceIndicator, cu
  * 5. PromptForBequeathingDataCausesALTbeingRequestedToStopNotificationsToOldRelease
  */
 async function redirectNotificationNewRelease(user, xCorrelator, traceIndicator, customerJourney) {
-  return new Promise(async function (resolve, reject) {
-    try {
-      await PromptForBequeathingDataCausesRObeingRequestedToNotifyApprovalsOfNewApplicationsToNewRelease(user, xCorrelator, traceIndicator, customerJourney);
-      await PromptForBequeathingDataCausesRObeingRequestedToNotifyWithdrawnApprovalsToNewRelease(user, xCorrelator, traceIndicator, customerJourney);
-      await PromptForBequeathingDataCausesALTbeingRequestedToNotifyLinkUpdatesToNewRelease(user, xCorrelator, traceIndicator, customerJourney)
-      await PromptForBequeathingDataCausesRObeingRequestedToStopNotificationsToOldRelease(user, xCorrelator, traceIndicator, customerJourney);
-      await PromptForBequeathingDataCausesALTbeingRequestedToStopNotificationsToOldRelease(user, xCorrelator, traceIndicator, customerJourney)
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+  await PromptForBequeathingDataCausesRObeingRequestedToNotifyApprovalsOfNewApplicationsToNewRelease(user, xCorrelator, traceIndicator, customerJourney);
+  await PromptForBequeathingDataCausesRObeingRequestedToNotifyWithdrawnApprovalsToNewRelease(user, xCorrelator, traceIndicator, customerJourney);
+  await PromptForBequeathingDataCausesALTbeingRequestedToNotifyLinkUpdatesToNewRelease(user, xCorrelator, traceIndicator, customerJourney)
+  await PromptForBequeathingDataCausesRObeingRequestedToStopNotificationsToOldRelease(user, xCorrelator, traceIndicator, customerJourney);
+  await PromptForBequeathingDataCausesALTbeingRequestedToStopNotificationsToOldRelease(user, xCorrelator, traceIndicator, customerJourney)
 }
 
 /**
@@ -104,15 +69,8 @@ async function redirectNotificationNewRelease(user, xCorrelator, traceIndicator,
  * 2. PromptForBequeathingDataCausesRequestForDeregisteringOfOldRelease
  */
 async function replaceOldReleaseWithNewRelease(user, xCorrelator, traceIndicator, customerJourney) {
-  return new Promise(async function (resolve, reject) {
-    try {
-      await promptForBequeathingDataCausesRequestForBroadcastingInfoAboutServerReplacement(user, xCorrelator, traceIndicator, customerJourney);
-      await promptForBequeathingDataCausesRequestForDeregisteringOfOldRelease(user, xCorrelator, traceIndicator, customerJourney);
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+  await promptForBequeathingDataCausesRequestForBroadcastingInfoAboutServerReplacement(user, xCorrelator, traceIndicator, customerJourney);
+  await promptForBequeathingDataCausesRequestForDeregisteringOfOldRelease(user, xCorrelator, traceIndicator, customerJourney);
 }
 
 /**
@@ -164,11 +122,11 @@ async function PromptForBequeathingDataCausesTransferOfListOfApplications(user, 
             requestBody,
             user,
             xCorrelator,
-            traceIndicator,
+            traceIndicator + "." + traceIncrementer++,
             customerJourney
           );
           if (!result) {
-            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
           }
 
         } catch (error) {
@@ -229,11 +187,11 @@ async function PromptForBequeathingDataCausesRObeingRequestedToNotifyApprovalsOf
           requestBody,
           user,
           xCorrelator,
-          traceIndicator,
+          traceIndicator + "." + traceIncrementer++,
           customerJourney
         );
         if (!result) {
-          throw forwardingKindNameOfTheBequeathOperation + " forwarding is not success for the input" + requestBody;
+          throw forwardingKindNameOfTheBequeathOperation + " forwarding is not success for the input" + JSON.stringify(requestBody);
         }
 
       } catch (error) {
@@ -293,11 +251,11 @@ async function PromptForBequeathingDataCausesRObeingRequestedToNotifyWithdrawnAp
           requestBody,
           user,
           xCorrelator,
-          traceIndicator,
+          traceIndicator + "." + traceIncrementer++,
           customerJourney
         );
         if (!result) {
-          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
         }
 
       } catch (error) {
@@ -357,11 +315,11 @@ async function PromptForBequeathingDataCausesALTbeingRequestedToNotifyLinkUpdate
           requestBody,
           user,
           xCorrelator,
-          traceIndicator,
+          traceIndicator + "." + traceIncrementer++,
           customerJourney
         );
         if (!result) {
-          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
         }
 
       } catch (error) {
@@ -424,11 +382,11 @@ async function PromptForBequeathingDataCausesRObeingRequestedToStopNotifications
             requestBody,
             user,
             xCorrelator,
-            traceIndicator,
+            traceIndicator + "." + traceIncrementer++,
             customerJourney
           );
           if (!result) {
-            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
           }
         }
 
@@ -483,11 +441,11 @@ async function PromptForBequeathingDataCausesALTbeingRequestedToStopNotification
           requestBody,
           user,
           xCorrelator,
-          traceIndicator,
+          traceIndicator + "." + traceIncrementer++,
           customerJourney
         );
         if (!result) {
-          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
         }
 
       } catch (error) {
@@ -552,11 +510,11 @@ async function promptForBequeathingDataCausesRequestForBroadcastingInfoAboutServ
           requestBody,
           user,
           xCorrelator,
-          traceIndicator,
+          traceIndicator + "." + traceIncrementer++,
           customerJourney
         );
         if (!result) {
-          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+          throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
         }
 
       } catch (error) {
@@ -610,11 +568,11 @@ async function promptForBequeathingDataCausesRequestForDeregisteringOfOldRelease
             requestBody,
             user,
             xCorrelator,
-            traceIndicator,
+            traceIndicator + "." + traceIncrementer++,
             customerJourney
           );
           if (!result) {
-            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + requestBody;
+            throw forwardingKindNameOfTheBequeathOperation + "forwarding is not success for the input" + JSON.stringify(requestBody);
           }
         }
       } catch (error) {
