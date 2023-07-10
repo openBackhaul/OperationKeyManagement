bca081583d23894337427f2eb713f7962069338a
Prathiba <prathiba.jeevan.external@telefonica.com>
2023-07-10 13:55:51 +0200

    Regard-application is failing because of including response.took Fixes #245

--- server/controllers/IndividualServices.js
+++ server/controllers/IndividualServices.js
@@ -76,7 +76,7 @@ module.exports.regardApplication = async function regardApplication(req, res, ne
       let responseBodyToDocument = undefined;
       await IndividualServices.regardApplication(body, user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
         .then(async function (responseBody) {
-          let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url, responseBody.took);
+          let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
           restResponseBuilder.buildResponse(res, responseCode, responseBodyToDocument, responseHeader);
         })
         .catch(async function (responseBody) {
@@ -96,7 +96,7 @@ module.exports.regardUpdatedLink = async function regardUpdatedLink(req, res, ne
     let responseBodyToDocument = undefined;
     await IndividualServices.regardUpdatedLink(body, user, originator, xCorrelator, traceIndicator, customerJourney)
       .then(async function (responseBody) {
-        let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url, responseBody.took);
+        let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
         restResponseBuilder.buildResponse(res, responseCode, responseBodyToDocument, responseHeader);
       })
       .catch(async function (responseBody) {
